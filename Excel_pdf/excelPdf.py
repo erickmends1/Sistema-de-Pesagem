385364e1773e620c66992778f831d096cd41f6b0
Erick Mendes <dev.erickmendes126@gmail.com>
2024-12-11 08:25:39 -0300

    Nova logica para excelPdf.py
    Utilização de uma nova biblioteca mais simplificada e mais otimizada pra o sistema.

--- Excel_pdf/excelPdf.py
+++ Excel_pdf/excelPdf.py
@@ -1,29 +1,11 @@
-import pandas as pd
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
+import aspose.cells
+from aspose.cells import Workbook 
 
 class Excel_para_Pdf:
-        def gerarPdf(caminho):
-    # Carregar o arquivo Excel (XLSX)
-            df = pd.read_excel(caminho)
+    def __init__(self):
+        pass
 
-            # Converter o DataFrame para string
-            table_str = df.to_string(index=False)
 
-            # Criar o PDF
-            pdf_file = "output.pdf"
-            c = canvas.Canvas(pdf_file, pagesize=letter)
-
-            # Definir a posição inicial no PDF
-            x = 40
-            y = 750
-
-            # Escrever a tabela no PDF
-            for line in table_str.split('\n'):
-                c.drawString(x, y, line)
-                y -= 12  # Desce para a próxima linha
-
-            # Salvar o PDF
-            c.save()
-
-            print(f"PDF gerado com sucesso: {pdf_file}")
+    def gerarPdf(caminho):
+        panilha = Workbook(caminho)
+        panilha.save("Ticket_pdf.pdf")
