4a98a7e458418e1451327e8bbdca58b5b28cbcec
Erick Mendes <dev.erickmendes126@gmail.com>
2025-01-06 08:37:25 -0300

    Update funcoes.py

--- Utilitarios/funcoes.py
+++ Utilitarios/funcoes.py
@@ -1,3 +1,16 @@
 
-from Entidades.Cliente.cliente import *
-from Entidades.Veiculos.veiculo import Veiculo
+
+def calcular(bruto, tara):
+    liquido = bruto - tara
+    return liquido
+
+def convert_to_float(numero):
+    try:
+        # Tenta converter para float
+        valor = float(numero)
+        return valor
+    except ValueError:
+        # Retorna None se o valor for inválido
+        return None
+            
+        
