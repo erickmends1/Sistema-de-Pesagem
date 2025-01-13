6826fa4ee433b9211a1bfd5354740e56f9aa4c12
Erick Mendes <dev.erickmendes126@gmail.com>
2025-01-13 17:34:51 -0300

    Update modificando_excel.py

--- Ticket/modificando_excel.py
+++ Ticket/modificando_excel.py
@@ -3,13 +3,19 @@ from .Excel_pdf.excel_para_pdf import Excel_para_Pdf
 
 class ModificExel:
 
-    def gerarExcel(cliente, placa, motorista, peso_bruto, tara, peso_liquido):
+    def gerarExcel(idTk, cliente, placa, motorista, peso_bruto, tara, peso_liquido):
 
         ticket_excel = 'Ticket/ticket_balanca.xlsx'
 
         planilha = load_workbook(ticket_excel)
 
         sheet = planilha.active
+        #Id
+        sheet['J4'] = idTk
+        sheet['C7'] = idTk
+        sheet['J21'] = idTk
+        sheet['C24'] = idTk
+
         #Cliente
         sheet['D9'] = cliente
 
